4376792e6d0a5b620fbb7d4a4030d328610d39e4
scott95100 <scott95100@gmail.com>
2021-02-23 14:42:55 -0700

    comment commponent added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Comment from './Comment';
 
 function App() {
   // name variables before return statement
@@ -8,6 +9,8 @@ function App() {
     body: "Check out this body property",
     comments: ['First!', 'Great post', 'Hire this aithor now!']
   }
+  const comments = post.comments.map((comment, index) => (<Comment text = {comment} key={index} />));
+
   return (
     <div className="App">
       {/* can use variables with curly brackets */}
@@ -15,6 +18,8 @@ function App() {
       <h2>by {post.author}</h2>
       <h2><b>Comments:</b></h2>
       <p>{post.comments[Math.floor(Math.random() * post.comments.length)]}</p>
+      
+      { comments }
 
     </div>
   );
