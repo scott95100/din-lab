8fedae0c5e31d3de9c462467ccb4b4dc699dcd9c
scott95100 <scott95100@gmail.com>
2021-02-23 13:00:09 -0700

    react component practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // name variables before return statement
+  const post = {
+    title: 'Dinosaurs are awesome',
+    author: 'Stealthy Stegosaurs', 
+    body: "Check out this body property",
+    comments: ['First!', 'Great post', 'Hire this aithor now!']
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* can use variables with curly brackets */}
+      <h1>{post.title}</h1>
+      <h2>by {post.author}</h2>
+      <h2><b>Comments:</b></h2>
+      <p>{post.comments[0]}</p>
+
     </div>
   );
 }
